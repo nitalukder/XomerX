40731626360870d6ed535ebe3b180078e68ce0a3
Md. Nurul Islam Talukder Nahid <61370261+nitalukder@users.noreply.github.com>
2021-10-09 21:30:25 +0600

    updated

---
+++ info.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * MyClass Class Doc Comment
+ *
+ *  PHP Version 8.0.1
+ * 
+ * @category Class
+ * @package  MyPackage
+ * @author   Md. Nurul Islam Talukder Nahid <nitalukder62443@gmail.com>
+ * @license  https://github.com/nitalukder/$licenceURL MIT License
+ * @link     https://nitalukder.github.io/$directoryName
+ */
+?>
+
+<?php
+
+// Name Strings
+$indexName = 'Welcome';
+$siteName = 'XomerX';
+
+// Special Strings
+$titleSeparators = ' by ';
+
+?>
---
+++ src/index.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * MyClass Class Doc Comment
+ *
+ *  PHP Version 8.0.1
+ * 
+ * @category Class
+ * @package  MyPackage
+ * @author   Md. Nurul Islam Talukder Nahid <nitalukder62443@gmail.com>
+ * @license  https://github.com/nitalukder/$licenceURL MIT License
+ * @link     https://nitalukder.github.io/$directoryName
+ */
+?>
+
+<!-- Required PHP Files -->
+<?php
+require_once '../info.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $indexName . $titleSeparators . $siteName; ?></title>
+</head>
+
+<body>
+
+</body>
+
+</html>
